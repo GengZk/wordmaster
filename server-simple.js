7902fd37d7ed06b110384da6c7f1153556309d29
GengZK <17732586849@163.com>
2025-07-05 20:48:51 +0800

    feat: 添加Glitch部署配置

--- server-simple.js
+++ server-simple.js
@@ -5,7 +5,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // 中间件
 app.use(cors());
